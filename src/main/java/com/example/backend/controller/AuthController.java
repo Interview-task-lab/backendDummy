415aa543d421515a811ac97d627364540ca25a76
ismailaslan <ismailaslan190525@gmail.com>
2026-04-27 23:51:35 +0300

    .

--- src/main/java/com/example/backend/controller/AuthController.java
+++ src/main/java/com/example/backend/controller/AuthController.java
@@ -33,4 +33,10 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
     }
+
+    @GetMapping("/dashbooard")
+    public ResponseEntity<?> dashboard() {
+        return ResponseEntity.status(HttpStatus.OK).body("Dashboard");
+    }
+
 }
